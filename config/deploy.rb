cf0eec0dea71fa47d92c1225c5f2a38eb9a297c5
guillaume belleguic <guillaume.belleguic@cixi.eu>
2009-04-29 21:38:20 +0200

    add tmp folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,17 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "downjustforme"
+set :repository,  "git@github.com:tetsuo692/downjustforme.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
+set :scm, :git
+set :scm_passphrase, 'zbmp47alye'
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+ssh_options[:forward_agent] = true
+
+role :app, "www.cixi.eu"
